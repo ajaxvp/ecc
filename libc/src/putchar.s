15d49fc316d10d191553d75b45874123dc9b28be
ajaxvp <ajax.peterson156@gmail.com>
2025-06-29 11:02:43 -0400

    oops

--- libc/src/putchar.s
+++ libc/src/putchar.s
@@ -4,8 +4,9 @@ putchar:
     pushq %rbp
 	movq %rsp, %rbp
     subq $16, %rsp
-    movb %dil, -1(%rbp)
-    leaq -1(%rbp), %rsi
+    movb %dil, -2(%rbp)
+    movb $0, -1(%rbp)
+    leaq -2(%rbp), %rsi
     movq $1, %rdx
     movq $1, %rax
     movq $1, %rdi
