18b6e57ef3fbfa629b9ad56ebf2c8de3b3552ed5
ajaxvp <ajax.peterson156@gmail.com>
2025-07-16 23:41:46 -0400

    starting on I/O stuff

--- libc/include/stdio.h
+++ libc/include/stdio.h
@@ -1,6 +1,15 @@
 #ifndef _STDIO_H
 #define _STDIO_H
 
+#define FOPEN_MAX 16
+
+typedef struct __ecc_file
+{
+    int __fd;
+} FILE;
+
+// FILE* fopen(const char* filename, const char* mode);
+
 int putchar(int c);
 
 int puts(const char* str);
--- libc/include/stdlib.h
+++ libc/include/stdlib.h
@@ -1,6 +1,8 @@
 #ifndef _STDLIB_H
 #define _STDLIB_H
 
+#define NULL ((void*) 0)
+
 int abs(int j);
 
 #endif
